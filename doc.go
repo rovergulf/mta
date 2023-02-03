efa361c3a5f22435a65cb90ad15f6f523aa50a44
Dmitrii Limonov <disordervlz@gmail.com>
2023-02-03 18:13:26 +0700

    Fix doc.go comments

--- doc.go
+++ doc.go
@@ -1,5 +1,5 @@
 // Package mta provides a simple interface to compose emails and to mail them
 // efficiently.
 //
-// More info on Github: https://github.com/emailia/pkg/mta
+// More info on Github: https://github.com/rovergulf/mta
 package mta
